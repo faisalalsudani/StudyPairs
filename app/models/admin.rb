7377d0e049b9250395529dcb2b99ab925b4f1be2
faisalalsudani <f4desiign@gmail.com>
2017-09-18 17:38:05 +0200

    Random name generator

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,26 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
+
+
+  def random_student
+    @students = Student.all
+    test_array = []
+    test_array = @students.ids
+
+    r1 = test_array.sample
+    test_array.delete(r1)
+
+    r2 = test_array.sample
+    test_array.delete(r2)
+
+    puts "The first pairs are #{r1} and #{r2}"
+    puts "*" * 50
+
+
+    puts test_array
+
+
+  end
+
 end
