960cca1c2d7382ff5a70971480e8d1f8ccccea78
SereneAngelina <sereneangelina1@gmail.com>
2017-09-18 15:47:58 +0200

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+faisal = Student.create!(username: "Faisal", email: "faisal@gmail.com", password: "123456", admin: true)
+serene = Student.create!(username: "Serene", email: "serene@gmail.com", password: "123456", admin: false)
+jane = Student.create!(username: "Jane", email: "jane@gmail.com", password: "123456", admin: false)
+ann = Student.create!(username: "Ann", email: "ann@gmail.com", password: "123456", admin: false)
+eden = Student.create!(username: "Eden", email: "eden@gmail.com", password: "123456", admin: false)
+amy = Student.create!(username: "Amy", email: "amy@gmail.com", password: "123456", admin: false)
+john = Student.create!(username: "John", email: "john@gmail.com", password: "123456", admin: false)
