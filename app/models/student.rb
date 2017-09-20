12b614dc898850eef516e63f16c5e39e37387e19
SereneAngelina <sereneangelina1@gmail.com>
2017-09-20 17:11:36 +0200

    Dont merge

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,8 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
-     
+
+    $student_array = Hash.new {|h,k| h[k] = Array.new }
   def random_student
            @students = Student.all
            total_students = @students.length
@@ -11,22 +12,34 @@ class Student < ApplicationRecord
            pairs = []
            #student_array = Hash.new {|h,k| h[k] = Array.new }
            test_array = @students.ids
-           i = 1
+           #i = 1
 
-             while i < total_students
-               r1 = test_array.sample
-                test_array.delete(r1)
-               r2 = test_array.sample
-                test_array.delete(r2)
-                #student_array[r1].push(r2)
 
-               pairs.push(Student.find_by(id:r1).username)
-               pairs.push(Student.find_by(id:r2).username)
+           for i in 0..total_students-1
+              for j in i+1..total_students-1
+                pairs.push(Student.find_by(id:test_array[i]).username + "|" + Student.find_by(id:test_array[j]).username)
+              end
+           end
 
-               i+= 2
-             end
-             #puts student_array
-               pairs
+            #  while i < total_students
+            #    r1 = test_array.sample
+             #
+            #     test_array.delete(r1)
+            #    r2 = test_array.sample
+             #
+            #     if $student_array[r1].include?(r2)
+            #       next
+            #     end
+            #     test_array.delete(r2)
+            #     $student_array[r1].push(r2)
+             #
+            #    pairs.push(Student.find_by(id:r1).username)
+            #    pairs.push(Student.find_by(id:r2).username)
+             #
+            #    i+= 2
+            #  end
+             #puts $student_array
+                pairs.first
        end
-    end
+
 end
