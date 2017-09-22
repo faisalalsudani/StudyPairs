fa21f7ffcc1f2f4b37a87ed442ac84753a5d74f0
SereneAngelina <sereneangelina1@gmail.com>
2017-09-22 15:09:56 +0200

    Additional test specs

--- spec/features/student/student_pairs_spec.rb
+++ spec/features/student/student_pairs_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe "Student view pairs for today and previous days" do
+  before { login_as student }
+
   let(:student) { create :student }
 
   it "shows the student details" do
-    visit student_url(student)
+    visit students_url(student)
 
     expect(page).to have_text(student.username)
+    expect(page).to have_text("Your match for today")
+    expect(page).to have_text("Your match for previous days")
 
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,16 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+require 'capybara/rails'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -33,7 +42,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -54,4 +63,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+
 end
